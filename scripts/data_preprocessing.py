7835c9aa151a307531b26851a879e0516d8a75b5
Nick-Kevin <nicblou23@gmail.com>
2025-12-16 02:20:28 +0400

    update : make data preprocessing done

--- scripts/data_preprocessing.py
+++ scripts/data_preprocessing.py
@@ -1,8 +1,6 @@
 import pandas as pd
-import os
 import joblib
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
 from sklearn.model_selection import train_test_split
@@ -28,3 +26,32 @@ categorical_features = X.select_dtypes(include=['object']).columns
 
 print('\nNumerical features:', numercial_features)
 print('\nCategorical features:', categorical_features)
+
+print('\n Info')
+print(X.info())
+
+numerical_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='mean')),
+    ('scaler', StandardScaler())
+])
+
+# process the features
+X_transformed = numerical_transformer.fit_transform(X)
+
+X_transformed_df = pd.DataFrame(X_transformed, columns=numercial_features)
+print('X transformed DataFrame')
+print(X_transformed_df.head())
+print('\nX transformed DataFrame description')
+print(X_transformed_df.describe())
+
+# Save the preprocessor object
+joblib.dump(numerical_transformer, 'models/preprocessor.pkl')
+
+# Split data into training and test sets after preprocessing
+X_train, X_test, y_train, y_test = train_test_split(X_transformed_df, y, test_size=0.2, random_state=42)
+
+# Save preprocessed data and split datasets to files
+pd.to_pickle(X_transformed_df, 'models/X_transformed.pkl')
+pd.to_pickle(y_test, 'models/y_test.pkl')
+
+print("Data preprocessing completed.")
